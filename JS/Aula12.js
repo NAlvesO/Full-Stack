a2bfc78d281fdfaca55e84cdd245f94a75e22000
Nilton Alves <niltonalves31@gmail.com>
2024-02-26 10:51:33 -0300

    Utilizacao while

--- JS/Aula12.js
+++ JS/Aula12.js
@@ -4,3 +4,24 @@ for (let pratica = 0; pratica < 5; pratica++)
 {
     console.log("O laco de repeticao atingiu: " + pratica)
 }
+
+
+//for outros aplicacoes
+
+for(let velocidade = 0; velocidade <= 11; velocidade++)
+{
+    velocidade += 1
+    console.log("O veiculo esta ganhando velocidade: " + velocidade)
+
+
+if(velocidade < 9)
+{
+    console.log("O veiculo esta dentro da velocidade da via!")
+    
+}
+else
+{
+    console.log("O limite de velocidade foi atingido: " + velocidade)
+}
+
+}
---
+++ JS/Aula13.js
@@ -0,0 +1,2 @@
+//while Alternativa "for"
+
