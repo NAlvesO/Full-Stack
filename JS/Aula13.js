4bc64a0ed7e91c88e33bde78fe60a1a3e14b77fe
Nilton Alves <niltonalves31@gmail.com>
2024-02-26 11:22:43 -0300

    Utilizacao do while

--- JS/Aula13.js
+++ JS/Aula13.js
@@ -1,2 +1,18 @@
-//while Alternativa "for"
+//while -Enquanto- Alternativa "for"
 
+let vida = 0      
+
+while(vida < 5)
+{
+    vida++
+}
+
+if(vida > 10)
+{
+    console.log("Sua vida esta em: " + vida)
+}
+
+else
+{
+    console.log("Voce esta morto!")
+}
