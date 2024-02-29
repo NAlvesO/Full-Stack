ee0c5c437c7ad914796112f3807346506ef9f7a5
Nilton Alves <niltonalves31@gmail.com>
2024-02-29 00:14:13 -0300

    Utilizando funcoes dentro de funcoes, para otimizar o codigo!

--- JS/Aula15.js
+++ JS/Aula15.js
@@ -12,3 +12,29 @@ function atigiu ()
 {
     console.log("Levando tiro!!")
 }
+
+///////////////////////////////////////////////////////////
+
+function barco()
+{
+    console.log("Navegando em ondas fortes.")
+}
+
+function marinheiros()
+{
+    console.log("Dirigindo o barco para nao bater nas rochas.")
+}
+
+function tempestade()
+{
+    console.log("Chuvas,trovoes, ondas gigantes!!!!!")
+}
+
+function main()
+{
+    barco()
+    marinheiros()
+    tempestade()
+}
+
+main()
