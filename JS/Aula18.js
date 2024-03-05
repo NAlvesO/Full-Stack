b4a2e3d626fd2310218fe7c643b4697d3f178315
Nilton Alves <niltonalves31@gmail.com>
2024-03-05 09:17:58 -0300

    Interpolacao de Funcoes - Utilizar ~~(cifrao)+{variavel~~  para add no texto!

---
+++ JS/Aula18.js
@@ -0,0 +1,9 @@
+//Interpolacao de Funcoes! - Para utilizar apenas colocor "${variavel}" para add no meio do texto!
+
+intepolation("Parametros","Nomes","Senhas aleatorias")
+
+function intepolation(intfunc, user, pass)
+{
+    console.log(`Utilizando a interpolacao para add ${intfunc} ou adcionar ${pass} ou 
+    ainda colocar ${user} `)
+}
