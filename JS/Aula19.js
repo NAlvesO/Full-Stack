e04f5404b4f3fa6f56b72ddc1e636cb34a96b4c4
Nilton Alves <niltonalves31@gmail.com>
2024-03-05 09:39:42 -0300

    Retorno de funcoes

---
+++ JS/Aula19.js
@@ -0,0 +1,10 @@
+//Retorno de Funcoes - Cada funcao pode retornar apenas 1 objeto/Valor
+
+let resultado = calculo(10,20)
+
+function calculo(A,B)
+{
+    return A + B
+}
+
+console.log(`O calculo vai ser igual: ${resultado} , com os valores passados!`)
