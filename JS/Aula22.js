13960c599e4c72382b5ef9cb18f22d12f8b46011
NILTON ALVES DE OLIVEIRA <t_nilton.oliveira@hapvida.com.br>
2024-03-21 15:57:03 -0300

    Orientação a objetos

---
+++ JS/Aula22.js
@@ -0,0 +1,12 @@
+class personalizacaoMoto 
+{
+    constructor(Pneu, Cor)
+    {
+        this.Pneu = Pneu
+        this.Cor = Cor
+    }
+}
+
+const motoFinal = new personalizacaoMoto ("Pneu 18", "Vermelha")
+
+console.log(motoFinal)
